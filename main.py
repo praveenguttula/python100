6bd16394ba4ca17d4f708eb2dd41bbb09fd1bc3c
praveenguttula <praveenguttula4@gmail.com>
2024-06-12 11:38:58 +0530

    guess.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print("display")
+
+guess = input("guess a letter:").lower()
+
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+    
+print(display)
